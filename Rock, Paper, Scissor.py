76301b6c69c5df823fd5c506723d1b2903300f31
jibson12 <50019512+jibson12@users.noreply.github.com>
2019-05-08 08:05:04 +0100

    Rock, Paper, Scissors
    Making the game rock, paper, scissors in Python. Mainly using if, else and return statements to make this game.

---
+++ Rock, Paper, Scissor.py
@@ -0,0 +1,30 @@
+#Making the game Rock, Paper, Scissors in Python
+
+user1 = input("Name of Player 1: ")
+user2 = input("Name of Player 2: ")
+user1_answer = input("%s, rock, paper or scissors?" % user1)
+user2_answer = input("%s, rock, paper or scissors?" % user2)
+
+def compare(u1, u2):
+    if u1 == u2:
+        return("It's a tie!")
+    elif u1 == 'rock':
+        if u2 == 'scissors':
+            return("Rock wins!")
+        else:
+            return("Paper wins!")
+    elif u1 == 'scissors':
+        if u2 == 'paper':
+            return("Scissors win!")
+        else:
+            return("Rock wins!")
+    elif u1 == 'paper':
+        if u2 == 'rock':
+            return("Paper wins!")
+        else:
+            return("Scissors win!")
+    else:
+        return("Invalid input! You have not entered rock, paper or scissors, try again.")
+        sys.exit()
+
+print(compare(user1_answer, user2_answer))
